bfef31d4fc35f056025da5fab91e00a9ae6c40db
pi <alber91@gmail.com>
2023-09-27 11:02:52 +0000

    Update web response

--- web.py
+++ web.py
@@ -5,34 +5,23 @@ import os
 from lxml.html import fromstring
 
 basepath = os.getcwd()
-p = subprocess.Popen(["pwd"])
-
-def enable_items():
-    k.set_text("")
-    i.enable()
-    j.enable()
-    os.chdir(basepath)
 
 def download(url, title):
-    newTitle = title.replace(" ", "_")
+    newTitle = title.replace(" ", "")
     try:
         os.mkdir(newTitle)
-        print("Created folder: " + newTitle)
     except OSError as error:
-        print(str(error))
+        False
 
     try:
         os.chdir(newTitle)
-        k.set_text("Running")
-        i.disable()
-        j.disable()
-        print("Started downloading: " + title)
-        ui.notify("Downloading: " + title)
         p = subprocess.Popen(["spotdl",url])
+        i.set_value("")
+        ui.notify("Downloading: " + title)
+        os.chdir(basepath)
     except Exception as e:
         ui.notify("Error: " + str(e))
-        print("Exception: " + str(e))
-        enable_items()
+        os.chdir(basepath)
 
 def url_ok(url):
     try:
@@ -55,11 +44,5 @@ with ui.column().classes('fixed-center').style('align-items: center;'):
         ui.link('Spoty', 'https://open.spotify.com/')
         i = ui.input(placeholder='Enter spoty url').props('rounded outlined dense').props('clearable')
         j = ui.button('Start Download', on_click=lambda: [url_ok(i.value)])
-        k = ui.label()
-
-poll = p.poll()
-if poll is not None:
-    print("Download finished")
-    enable_items()
 
 ui.run(port=3344, title="SpotWebDL", dark=None)
